798de243dddf05414c1368fbfeb785dbde9c13b7
hsynarslan <hsyn06arslan@gmail.com>
2022-01-08 00:59:38 +0300

    app initializing

---
+++ src/Components/BlogCard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const BlogCard = () => {
+  return (
+    <div>
+      <h1>BlogCard</h1>
+    </div>
+  );
+};
+
+export default BlogCard;
---
+++ src/Components/BlogForm.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const BlogForm = () => {
+  return (
+    <div>
+      <h1>BlogForm</h1>
+    </div>
+  );
+};
+
+export default BlogForm;
